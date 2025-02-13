7faacf938cc14c843264503b3b4d7ba9096828f0
thearhansheth <arhan.sheth@gmail.com>
2025-02-13 16:08:29 +0530

    Updated with /hang routine to simulate a crash

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,4 +1,5 @@
 import os
+import time
 from flask import Flask, make_response, request
 
 app = Flask(__name__)
@@ -29,10 +30,15 @@ def health():
     )
     return response
 
+@app.route('/hang')
+def hang():
+    while True:
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
     port = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=False)
